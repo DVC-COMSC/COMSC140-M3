2bb95a589084483e53b5550e399be4c7143eca65
Kyuwoong Lee <kwooler@gmail.com>
2022-10-10 19:40:40 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+
+
+## Complete your main.py 
+
+
+![M3](https://nimbus-screenshots.s3.amazonaws.com/s/5eb612cc02137b43360e0306d2125aaa.png)
+
+
+> > Ouput Eample 
+> > > [2, 8, 6, 10, 4, 9, 5, 7] 
---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+
+number = list(map(int, input().split()))
+
+repeatN = int(math.log2(len(number)))
+# print ('Number of iterations ', N)
+
+		
+# #####################
+# Make your coe
+# #####################
+
+
+print (number)
